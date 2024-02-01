4711388e89f249bc08cd13ebec066b33c64b4d44
Ishi-eenn <syi378038@gmail.com>
2024-02-01 16:25:22 +0900

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,63 +1,37 @@
-import { useState } from "react";
-import "./Search.css";
-import { postToDoList } from "../api.js";
-
-import { Container, Fab, TextField,
+import { useRef, useState } from 'react'
+import { Container, TextField,
   Button, Typography, Box, 
-  Grid, Card, CardMedia, CardContent, CardActions
+  Grid, Card, CardMedia, CardContent
 } from '@mui/material'
 
-function PrintSearchBooks({ searchResult }) {
-  return (
-    <div className="cardList">
-      {searchResult.map((data, index) => {
-        return (
-          <div key={index} className="book">
-            <img src={data.volumeInfo.imageLinks.thumbnail} />
-            <div className="title">{data.volumeInfo.title}</div>
-            <div className="authors">{data.volumeInfo.authors}</div>
-            <div className="description">{data.volumeInfo.description}</div>
-            <button onClick={async () => {
-              const book = {
-                title: data.volumeInfo.title,
-                description: data.volumeInfo.description,
-                url: data.volumeInfo.imageLinks.thumbnail,
-              };
-              await postToDoList(book);
-            }}>追加</button>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+const BookSearch = () => {
+  const keyword = useRef('')  
+  const [searchResult, setSearchResult] = useState([])
 
-function Search() {
-  const [input, setInput] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
+  const search = async (keyword, e) => {
+    e.preventDefault()
+  
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword.current.value}`)
+      .then( response => response.json())
+    console.log(response.items)
 
-  const bookDataFromUrl = async (string) => {
-    try {
-      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${string}&&cnt=1`);
-      if (!response.ok) {
-        throw new Error("もしかして上限きちゃった？");
-      }
-
-      const data = await response.json();
-      console.log(data);
-
-      if (data.items) {
-        setSearchResult(data.items);
-      } else {
-        setSearchResult([]);
-      }
-    } catch (error) {
-      console.error("値が取れなかったああああああ", error);
-    }
+  const newList = []
+  response.items.map( book => {
+  const title = book.volumeInfo.title
+  const img = book.volumeInfo.imageLinks
+  const description = book.volumeInfo.description
+  newList.push({ title: title ? title : '',
+    image: img ? img.thumbnail : '',
+    description: description ? description.slice(0, 40) : ''
+  })
+setSearchResult(newList) })
   }
 
-  return (
-    <>
+  const addBook = () => {
+    console.log('btn clicked')
+  }
+
+  return (<>
     <Container component="section" maxWidth="lg">
       <Box
         sx={{
@@ -68,13 +42,15 @@ function Search() {
         }}
       >
         <Typography component="h1" variant="h5">本を検索</Typography>
-        <Box component="form" onSubmit={ ()=> console.log("btn")} 
+        <Box component="form" onSubmit={ e => search(keyword, e) }
         sx={{ mt: 1}}>
           <TextField
+            required
             fullWidth
             label="本を検索"
             placeholder="本のタイトルを入力してください"
             name="search"
+            inputRef={keyword}
           />
           <Button
             type="submit"
@@ -86,10 +62,37 @@ function Search() {
         </Box>
       </Box>
     </Container>
-    </>
-  );
+    <Container component="section" maxWidth="lg">
+      <Grid container spacing={4}>
+        { searchResult.map((card, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} >
+            <Card sx={{ height: '100%'}}>
+              <Grid container>
+                <Grid item sm={4}>
+                  <CardMedia
+                    component="img"
+                    image={card.image}
+                    alt={card.title}
+                    />
+                </Grid>
+                <Grid item sm={8}>
+                  <CardContent>
+                    <Typography sx={{ fontSize: '16px'}}>
+                      { card.title}
+                    </Typography>
+                    <Typography sx={{ fontSize:'14px', mb: 1.5 }}>
+                      { card.description }
+                    </Typography>
+                    <Button variant="contained" size="small" onClick={() => addBook()}>追加</Button>
+                  </CardContent>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  </>)
 }
 
-export default Search;
-
-
+export default BookSearch
